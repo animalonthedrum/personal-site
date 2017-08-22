27499cbad9157f97983b9013c6306322cc52d466
Brett Miller <miller.brett.andrew@gmail.com>
2017-08-21 19:28:04 -0500

    added tooltip

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
-
-
+  // vex.defaultOptions.className = 'vex-theme-os';
+  // vex.dialog.alert({
+  //   // message: 'WELCOME TO MY SITE',
+  //   content: 'THANK YOU AND PLEASE CONTACT ME',
+  //
+  //   className: 'vex-theme-flat-attack' // Overwrites defaultOptions
+  // });
 });
